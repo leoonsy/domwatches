645efe3bd3fb24edbe80bfaff4ae7eead5ad89c1
leoonsy <yashanovsky.sergey@ya.ru>
2019-10-16 00:14:44 +0500

    add advantages and characters

--- public_html/test/src/index.php
+++ public_html/test/src/index.php
@@ -2,7 +2,7 @@
 <html lang="ru">
 
 <head>
-    <title><?= $title; ?>Frontend test</title>
+    <title>Domwatches</title>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
     <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" rel="stylesheet">
@@ -31,25 +31,26 @@
                 </div>
                 <ul class="navbar__top-menu">
                     <li class="navbar__item">
-                        <a class="navbar__link navbar__active" href="#">Главная</a>
+                        <a class="navbar__link navbar__active" href="#">ГЛАВНАЯ</a>
                     </li>
                     <li class="navbar__item">
-                        <a class="navbar__link" href="#">Заказать</a>
+                        <a class="navbar__link" href="#">ТОВАРЫ</a>
                     </li>
                     <li class="navbar__item">
-                        <a class="navbar__link" href="#">Видео</a>
+                        <a class="navbar__link" href="#">ВИДЕО</a>
                     </li>
                     <li class="navbar__item">
-                        <a class="navbar__link" href="#">Фото</a>
+                        <a class="navbar__link" href="#">ФОТО</a>
                     </li>
                     <li class="navbar__item">
-                        <a class="navbar__link" href="#">Доставка</a>
+                        <a class="navbar__link" href="#">ДОСТАВКА</a>
                     </li>
                     <li class="navbar__item">
-                        <a class="navbar__link" href="#">Контакты</a>
+                        <a class="navbar__link" href="#">КОНТАКТЫ</a>
                     </li>
                 </ul>
                 <i class="navbar__hamburger fa fa-bars"></i>
+                <a class="navbar__cart fa fa-shopping-cart" href="#"></a>
             </nav>
         </div>
     </header>
@@ -57,10 +58,127 @@
         <div class="container h-100 d-flex">
             <div class="top-info">
                 <h1 class="top-info__title">Современные мужские часы DOM</h1>
-                <h5 class="top-info__desc">Водонепроницаемые наручные часы с хронографом</h5>
+                <h2 class="top-info__desc">Наручные металлические и кожаные часы с хронографом</h2>
                 <hr class="top-info__hr" />
-                <p class="top-info__desc-2">Кварцевые часы с натуральным кожаным ремешком и нержавеющей сталью</p>
-                <a class="top-info__buy" href="#" role="button">Заказать часы</a>
+                <p class="top-info__desc-2">Стильные, устойчивые к царапинам водонепроницаемые часы DOM прослужат вам многие годы и подчеркнут вашу индивидуальность!</p>
+                <a class="top-info__buy" href="#" role="button">Выбрать часы</a>
+            </div>
+        </div>
+    </section>
+    <section id="section-advant">
+        <div class="container">
+            <div class="row advant">
+                <div class="col-lg-12 advant__header">
+                    <h3>ПРЕИМУЩЕСТВА ЧАСОВ</h3>
+                </div>
+                <div class="col-lg-4 advant__item">
+                    <div class="advant__img advant__glass"></div>
+                    <h4>Устойчивость к царапинам</h4>
+                    <p>Закаленное минеральное стекло имеет высокую прочность и сопротивление к царапинам. Данное покрытие сложно поцарапать даже намеренно.</p>
+                </div>
+                <div class="col-lg-4 advant__item">
+                    <div class="advant__img advant__protection"></div>
+                    <h4>Защита от повреждений</h4>
+                    <p>Нержавеющая сталь металлических часов защищает их от окисления и внешних повреждений.</p>
+                </div>
+                <div class="col-lg-4 advant__item">
+                    <div class="advant__img advant__design"></div>
+                    <h4>Безупречный дизайн</h4>
+                    <p>Внешний вид часов отлично дополнит образ владельца и придаст статусности</p>
+                </div>
+            </div>
+        </div>
+    </section>
+    <hr />
+    <section id="section-characters">
+        <div class="container">
+            <div class="row characters">
+                <div class="col-lg-12 characters__header">
+                    <h3>ХАРАКТЕРИСТИКИ ЧАСОВ</h3>
+                </div>
+                <div class="col-lg-6 characters__carousel">
+                    <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+                        <ol class="carousel-indicators">
+                            <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+                            <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+                            <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+                            <li data-target="#carouselExampleIndicators" data-slide-to="3"></li>
+                            <li data-target="#carouselExampleIndicators" data-slide-to="4"></li>
+                            <li data-target="#carouselExampleIndicators" data-slide-to="5"></li>
+                            <li data-target="#carouselExampleIndicators" data-slide-to="6"></li>
+                            <li data-target="#carouselExampleIndicators" data-slide-to="7"></li>
+                        </ol>
+                        <div class="carousel-inner">
+                            <div class="carousel-item active">
+                                <img class="d-block w-100" src="/test/dist/images/carousel-characters/1.jpg" alt="M-635BK-1M">
+                                <div class="carousel-caption d-none d-md-block">
+                                    <h3>M-635BK-1M</h3>
+                                </div>
+                            </div>
+                            <div class="carousel-item">
+                                <img class="d-block w-100" src="/test/dist/images/carousel-characters/2.jpg" alt="M-635BK-7M">
+                                <div class="carousel-caption d-none d-md-block">
+                                    <h3>M-635BK-7M</h3>
+                                </div>
+                            </div>
+                            <div class="carousel-item">
+                                <img class="d-block w-100" src="/test/dist/images/carousel-characters/3.jpg" alt="M-635D-1M9">
+                                <div class="carousel-caption d-none d-md-block">
+                                    <h3>M-635D-1M9</h3>
+                                </div>
+                            </div>
+                            <div class="carousel-item">
+                                <img class="d-block w-100" src="/test/dist/images/carousel-characters/4.jpg" alt="M-635D-7M">
+                                <div class="carousel-caption d-none d-md-block">
+                                    <h3>M-635D-7M</h3>
+                                </div>
+                            </div>
+                            <div class="carousel-item">
+                                <img class="d-block w-100" src="/test/dist/images/carousel-characters/5.jpg" alt="M-635BL-1M">
+                                <div class="carousel-caption d-none d-md-block">
+                                    <h3>M-635BL-1M</h3>
+                                </div>
+                            </div>
+                            <div class="carousel-item">
+                                <img class="d-block w-100" src="/test/dist/images/carousel-characters/6.jpg" alt="M-635BL-7M">
+                                <div class="carousel-caption d-none d-md-block">
+                                    <h3>M-635BL-7M</h3>
+                                </div>
+                            </div>
+                            <div class="carousel-item">
+                                <img class="d-block w-100" src="/test/dist/images/carousel-characters/7.jpg" alt="M-635L-1M9">
+                                <div class="carousel-caption d-none d-md-block">
+                                    <h3>M-635L-1M9</h3>
+                                </div>
+                            </div>
+                            <div class="carousel-item">
+                                <img class="d-block w-100" src="/test/dist/images/carousel-characters/8.jpg" alt="M-635L-7M">
+                                <div class="carousel-caption d-none d-md-block">
+                                    <h3>M-635L-7M</h3>
+                                </div>
+                            </div>
+                        </div>
+                        <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+                            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                        </a>
+                        <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+                            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                        </a>
+                    </div>
+                </div>
+                <div class="col-lg-6 characters__info">
+                    <ul class="characters__list">
+                        <li class="characters__item"><b>Модель:</b><span>M-635</span></li>
+                        <li class="characters__item"><b>Производитель:</b><span>Китай</span></li>
+                        <li class="characters__item"><b>Механизм:</b><span>Кварцевый</span></li>
+                        <li class="characters__item"><b>Корпус:</b><span>Нержавеющая сталь</span></li>
+                        <li class="characters__item"><b>Ремешок:</b><span>Нержавеющая сталь / кожа</span></li>
+                        <li class="characters__item"><b>Стекло:</b><span>Закаленное минеральное</span></li>
+                        <li class="characters__item"><b>Водонепроницаемость:</b><span>30м</span></li>
+                        <li class="characters__item"><b>Диаметр:</b><span>42мм</span></li>
+                        <li class="characters__item"><b>Год:</b><span>2019</span></li>
+                    </ul>
+                </div>
             </div>
         </div>
     </section>
